651bd398d3a470c9b6181fb208f892b96d4546da
yashgrover48 <43803298+yashgrover48@users.noreply.github.com>
2019-05-09 11:48:09 +0530

    Add files via upload

---
+++ src/main/java/com/demo/controller/CustomerController.java
@@ -0,0 +1,18 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomerController {
+	
+	@GetMapping("/spring")
+	public String showSpring(){
+		return "hello-world";
+	}
+	
+	@GetMapping("/")
+	public String showHome(){
+		return "home";
+	}	
+}
